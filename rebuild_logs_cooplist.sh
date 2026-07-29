096ad041dd2b1debc3262ae1dc5078aae02edaa3
csiemann <caetanosiemann@gmail.com>
2026-07-28 23:32:43 -0300

    Add: bash to rebuild and show logs

---
+++ rebuild_logs_cooplist.sh
@@ -0,0 +1,6 @@
+docker stop cooplist-app-container
+docker rm cooplist-app-container
+docker build -t cooplist-app:latest .
+docker run -d -p 5173:3000 --env-file .env --name cooplist-app-container cooplist-app:latest
+docker logs -f cooplist-app-container
+
